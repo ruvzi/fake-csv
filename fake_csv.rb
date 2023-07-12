7e824cdd3d1938fb2d010c51d640e2d0cd42ef95
Ruslan Zavatskyi <r.zavatskyi@amoniac.eu>
2023-07-12 22:20:19 +0300

    add generate fake csv

--- fake_csv.rb
+++ fake_csv.rb
@@ -18,22 +18,32 @@ module Psych
   end
 end
 
+def write_csv(rows, data, name)
+  fake_data = []
+
+  rows.times do |i|
+    fake = FakeData.new(data, i + 1)
+    fake_data << fake.fake_row
+  end
+
+  headers = data.each.with_object(:name).map(&:[])
+
+  fake_csv = CsvData.new(headers, fake_data, "csv/#{name}.csv")
+  fake_csv.write
+end
+
 name = ARGV[0] || 'demo'
+fake_files = ARGV[1]
 
 yaml_contents = File.read("templates/#{name}.yml")
 template = YAML.safe_load(yaml_contents, symbolize_names: true)
 
 rows = template[:rows]
 data = template[:data]
-
-fake_data = []
-
-rows.times do |i|
-  fake = FakeData.new(data, i + 1)
-  fake_data << fake.fake_row
+if fake_files
+  rows.times do |i|
+    write_csv(rows, data.shuffle, "fake_files/#{name}_#{i + 1}")
+  end
+else
+  write_csv(rows, data, name)
 end
-
-headers = data.each.with_object(:name).map(&:[])
-
-fake_csv = CsvData.new(headers, fake_data, "csv/#{name}.csv")
-fake_csv.write
